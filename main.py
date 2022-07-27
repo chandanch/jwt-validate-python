a0a32a42fd51c424b105dbda4096948696acac37
chandanch <chandan.cec197@gmail.com>
2022-07-27 15:49:35 +0530

    include headers and token expiry

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import os
 import jwt
 from dotenv import load_dotenv
 import base64
+import datetime
+import time
+
+import calendar
 
 load_dotenv()
 
@@ -14,16 +18,31 @@ decoded = jwt.decode(token, public_key, verify=False, algorithms=['RS256'])
 
 # print('Decoded: {}'.format(decoded))
 
+date = datetime.datetime.utcnow()
+print('Current Date time {}'.format(date));
+expiry_date = date + datetime.timedelta(minutes=3);
+print('Time Delta {}'.format(expiry_date));
+
 # Encode token based on data
 payload = {
     'name': decoded['name'],
-    'email': decoded['preferred_username'],
+    'email': decoded['upn'],
     'id': 222332,
     'roles': ['Admin'],
     'portfolio_access': [122,223, 333],
-    'is_active': True
+    'is_active': True,
+    'iss': 'https://camer.maindenierapp.net/DFiod659039-994ff/',
+    'exp': calendar.timegm(expiry_date.utctimetuple()),
+    'iat': calendar.timegm(date.utctimetuple()),
+    'nbf': calendar.timegm(expiry_date.utctimetuple())
 }
 
+
+token_headers = {
+    "kid": "2ZQpJ3UpbjAYXYGaXEJl8lV0TOICTDEV",
+    "nonce": "SSWUIPEXW2390DF",
+
+}
 
 jwt_private_key = os.environ.get('JWT_KEY')
 
@@ -41,12 +60,15 @@ jwt_private_key = os.environ.get('JWT_KEY')
 
 jwt_public_key = os.environ.get('JWT_PUB_KEY')
 
+# Creates a access token
+access_token = jwt.encode(payload=payload, key=base64.b64decode(jwt_private_key), algorithm='RS256', headers=token_headers)
 
-encoded = jwt.encode(payload=payload, key=base64.b64decode(jwt_private_key), algorithm='RS256')
-
-print('Encoded: {}'.format(encoded))
+print('Encoded: {}'.format(access_token))
 
 ## Decode encoded token
 print('Decoding encoded token...')
-enc_decode = jwt.decode(encoded, base64.b64decode(jwt_public_key), algorithms=['RS256'])
-print('Decoded Token: {}'.format(enc_decode));
+try:
+    enc_decode = jwt.decode(access_token, base64.b64decode(jwt_public_key), algorithms=['RS256'], options={"verify_signature": True})
+    print('Decoded Token: {}'.format(enc_decode));
+except jwt.ExpiredSignature:
+    print('Signature expired')
